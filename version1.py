4109c527dec16ff1c87133020dfdfed6fb4a903a
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 09:11:52 +0530

    Completed Version 4: User logging

--- version1.py
+++ version1.py
@@ -1,12 +1,15 @@
 pipeline {
     agent any
+    parameters {
+        string(name: 'USERNAME', defaultValue: 'Sharmila', description: 'Enter the username to store')
+    }
     stages {
-        stage('Version 3: File Handling') {
+        stage('Version 4: User Logging') {
             steps {
                 checkout scm
-
-                bat "echo Experiment 1 Version 3 Result > output.txt"
-                bat "type output.txt"
+                echo "Username has been successfully saved to user.txt"
+                
+                bat "type user.txt"
             }
         }
     }
