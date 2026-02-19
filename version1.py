27d7b7f074943de1e3366a892b78d4a3a8ebd093
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-02-19 09:40:57 +0530

    Version 1 complete

--- version1.py
+++ version1.py
@@ -1,28 +1,16 @@
 pipeline {
     agent any
-    
     parameters {
-        // This creates the input box in Jenkins
-        string(name: 'USERNAME', defaultValue: 'Admin', description: 'Enter your name to save to a file')
+        // Task 2: Accept VERSION parameter
+        string(name: 'VERSION', defaultValue: '1.0', description: 'Enter Build Version')
     }
-    
     stages {
-        stage('Task 1: Checkout') {
+        stage('Version 3') {
             steps {
-                // Pulls your latest code from GitHub
                 checkout scm
-            }
-        }
-        
-        stage('Task 2 & 3: File Operations') {
-            steps {
-                // Writes the parameter value into a file called user.txt
-                bat "echo ${params.USERNAME} > user.txt"
-                
-                echo "Success: Username has been saved."
-                
-                // Reads the file back to the console to prove it worked
-                bat "type user.txt"
+                // Task 3: Create version.txt
+                bat "echo ${params.VERSION} > version.txt"
+                bat "type version.txt"
             }
         }
     }
